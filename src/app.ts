944c5513e90a29b75e0f0ae9f8501175b3e219a0
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2024-05-07 20:43:19 +0700

    feat: add people route

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@
 import express, { Express } from "express";
 
 // IMPORT ROUTES
+import peopleRoute from "./route/peopleRoute";
 
 // IMPORT CONTROLLER AND HANDLER
 import { globalErrorMiddleware } from "./middleware/errorMiddleware";
+import { ResponseError } from "./utils/ResponseError";
 
 export const app: Express = express();
 
@@ -13,6 +15,15 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 // USE ROUTE
+app.use("/api/v1/people", peopleRoute);
+
+app.all("*", (req, res, next) => {
+  const err = new ResponseError(
+    "The resource requested could not be found on the server",
+    404
+  );
+  next(err);
+});
 
 // GLOBAL ERROR HANDLER
 app.use(globalErrorMiddleware);
